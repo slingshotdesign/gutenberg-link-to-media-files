42e03e7678b8b1cd7030e12fa46cd02c75c2fb86
Slingshot Design <mb@slingshot.co.uk>
2023-03-17 07:34:03 +0000

    Re-adding installable plugin files
    Previous files uploaded where source files with the plugin in a separate zip file and did not an install properly. These are the plugin file sonly so should install correctly.

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-rich-text'), 'version' => '15fb7b3e520fcbb7b196');
---
+++ slingshot-block-linktomedia.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Plugin Name:       Slingshot link to media files
+ * Description:       Adds a Media button to the RichText Toolbar in Gutenberg
+ * Requires at least: 6.1
+ * Requires PHP:      7.0
+ * Version:           1.0.1
+ * Author: Slingshot Design
+ * Author URI: http://www.slingshot.co.uk
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * @package           slingshot-block-linktomedia
+ */
+
+/**
+ * Register block using the metadata loaded from the block.json file.
+ *
+ * @link https://developer.wordpress.org/reference/functions/register_block_type/
+ * ----------------------------------------------------------------------
+ */
+if ( ! function_exists( 'slingshot_addmedia_block_init' ) ) {
+    function slingshot_addmedia_block_init() {
+        register_block_type( __DIR__ . '/build' );
+    }
+}
+add_action( 'init', 'slingshot_addmedia_block_init' );
